1cb5e425631b529ecda6461af9fa9ec85c953283
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2026-06-13 12:00:48 -0600

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.end=False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root=TrieNode()
+        
+    def addWord(self, word: str) -> None:
+        cur=self.root
+        for letter in word:
+            if letter not in cur.children:
+                cur.children[letter]=TrieNode()
+            cur=cur.children[letter]
+        cur.end=True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, cur):
+            for i in range(j, len(word)):
+                letter=word[i]
+                if letter == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if letter not in cur.children:
+                        return False
+                    cur=cur.children[letter]
+            return cur.end        
+        return dfs(0, self.root)
+        
