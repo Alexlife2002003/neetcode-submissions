8d2b249bea016d2e387eec9c40844356567cbde1
Alexia Hernandez Martinez <89033131+Alexlife2002003@users.noreply.github.com>
2026-07-06 12:28:08 -0600

    Update: lru-cache - submission-2

--- Data Structures & Algorithms/lru-cache/submission-2.py
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -4,29 +4,29 @@ class Node:
         self.val=val
         self.prev=None
         self.next=None
-        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity=capacity
+        self.cap=capacity
         self.cache={}
-
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.left=Node(0,0)
+        self.right=Node(0,0)
         self.left.next=self.right
-        self.right.prev = self.left
+        self.right.prev=self.left
     
     def insert(self, node):
         prev=self.right.prev
         nxt=self.right
-        prev.next = nxt.prev = node
-        node.next=nxt
         node.prev=prev
+        node.next=nxt
+        nxt.prev=prev.next=node
     
     def remove(self, node):
         prev=node.prev
         nxt=node.next
-        prev.next = nxt  # Correct: Bypass the node
-        nxt.prev = prev
+        prev.next=nxt
+        nxt.prev=prev
         
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -42,8 +42,7 @@ class LRUCache:
         
         self.cache[key]=Node(key, value)
         self.insert(self.cache[key])
-
-        if len(self.cache)>self.capacity:
+        if len(self.cache)>self.cap:
             lru=self.left.next
             self.remove(lru)
             del self.cache[lru.key]
